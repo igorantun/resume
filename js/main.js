cd9c32df2656d965b8a37afb86ddbeb92740dec3
Igor Antun <igorantunc@hotmail.com>
2015-04-21 23:31:04 -0300

    Start repo

---
+++ js/main.js
@@ -0,0 +1,38 @@
+var myself = {
+    name: 'Igor Antun',
+    age: 15,
+    country: 'Brazil',
+    languages: ['pt-BR', 'en-US']
+};
+
+var languages = [
+    'Javascript',
+    'Python',
+    'C++'
+];
+
+var frameworks = [
+    'jQuery',
+    'Express',
+    'Atom-Shell (Eletron)',
+    'NW.JS'
+];
+
+var projects = {
+    'node-chat': 'https://github.com/IgorAntun/node-chat',
+    'unsplashify': 'https://github.com/IgorAntun/unsplashify',
+    'node-slack-irc': 'https://github.com/IgorAntun/node-slack-irc'
+};
+
+var social = {
+    'email': 'igorantunc@hotmail.com',
+    'facebook': 'https://facebook.com/igorantunc',
+    'twitter': 'https://twitter.com/igorantun',
+    'github': 'https://github.com/igorantun',
+    'stackoverflow': 'https://stackoverflow.com/users/4677119/igor-antun'
+};
+
+var translator = [
+    'plug.dj',
+    'PopcornTime'
+];
